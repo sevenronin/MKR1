ec7409a3ce55fabb49c8f9ef4050bf70b21bc529
Ember <plahina01@mail.ru>
2021-06-11 21:00:58 +0500

    Удаляю лишние подключаемые библиотеки
    Были удалены неиспользуемые библиотеки для упрощения внешнего вида кода

--- movieRoller/MainWindow.xaml.cs
+++ movieRoller/MainWindow.xaml.cs
@@ -1,34 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using TMDbLib;
 using TMDbLib.Client;
 using TMDbLib.Objects.Discover;
 using TMDbLib.Objects.General;
-using TMDbLib.Objects.Genres;
-using TMDbLib.Objects.Movies;
 using TMDbLib.Objects.Search;
-using TMDbLib.Utilities.Converters;
 
 namespace movieRoller
 {
